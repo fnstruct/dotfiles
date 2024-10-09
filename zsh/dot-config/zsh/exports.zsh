621217f3f24d9e1e8d04c0e3d88e671bf2458329
fnstruct <184018508+fnstruct@users.noreply.github.com>
2024-10-09 01:50:00 -0400

    zsh: Configure shell exports

---
+++ zsh/dot-config/zsh/exports.zsh
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: MIT
+
+# Use different history file
+export HISTFILE="$HOME/.zshhistory"
+
+# Increase default history size
+export HISTSIZE=3000
+export SAVEHIST="$HISTSIZE"
+
+convey-on-availability() {
+    if command -v "$2" &>/dev/null; then
+        export "$1=$2"
+    fi
+}
+
+# Use less as the default PAGER
+convey-on-availability "PAGER" "less"
+
+# Use micro as default EDITOR and VISUAL
+convey-on-availability "EDITOR" "micro"
+convey-on-availability "VISUAL" "$EDITOR"
