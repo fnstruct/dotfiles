92c0085ffb4d232aa4930d9839f62f59060c2f87
fnstruct <184018508+fnstruct@users.noreply.github.com>
2024-10-11 18:52:00 -0400

    zsh: Cleanup configuration

--- zsh/dot-config/zsh/backdrop.zsh
+++ zsh/dot-config/zsh/backdrop.zsh
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: MIT
 
 # Pre-append directories to PATH
-# See: github.com/jonpalmisc/dotfiles
+# See: https://github.com/jonpalmisc/dotfiles
 
-path_prepend() {
-	[ -d "$1" ] && path=($1 $path)
+path-prepend() {
+    [ -d "$1" ] && path=($1 $path)
 }
 
 # Homebrew
-path_prepend "/opt/homebrew/sbin"
-path_prepend "/opt/homebrew/bin"
+path-prepend "/opt/homebrew/sbin"
+path-prepend "/opt/homebrew/bin"
