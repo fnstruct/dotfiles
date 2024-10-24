d278f2b7e4468dcb0797b06cafd990ff7bed63cb
fnstruct <184018508+fnstruct@users.noreply.github.com>
2024-10-23 23:48:00 -0400

    brew: Configure minimal Homebrew setup

---
+++ brew/minimal.rb
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: MIT
+
+# Minimal dependencies required in any given environment
+# Mostly helpful when setting up a (new) (virtual) machine
+
+brew "bat"
+brew "editorconfig"
+brew "git"
+brew "gnu-sed"
+brew "lf"
+brew "lsd"
+brew "micro"
+brew "stow"
+brew "zsh-syntax-highlighting"
